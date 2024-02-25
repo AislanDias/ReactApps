0cc5cd8876b9024bc448188acbb8fe6ead73caae
Aislan Dias <aislandias@outlook.com>
2024-02-25 17:35:08 -0300

    0104-styled-components

---
+++ 02-timer/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./componets/Button";
+
+export function App() {
+
+  return (
+    <>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button/>
+    </>
+  )
+}
+
