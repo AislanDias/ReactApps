b5e51cf16f89c18ad4ff6786bd9f6a9b1bec7c34
Aislan Dias <aislandias@outlook.com>
2024-02-26 16:25:58 -0300

    0109-setting-up-eslint

--- 02-timer/src/@types/styles.d.ts
+++ 02-timer/src/@types/styles.d.ts
@@ -2,7 +2,7 @@ import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
 // Creating a type from a infered typescript object
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 // We are overwriting some styled-components types ts declarations from this file
 declare module 'styled-components' {
--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,20 +1,18 @@
-import { Button } from "./componets/Button";
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { Button } from './componets/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   // We can use ThemeProvider as a way to switch between light and dark themes
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button/>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button />
 
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
