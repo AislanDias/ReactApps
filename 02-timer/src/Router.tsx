04a9279b7bb400e3e9f50394ccfbb18f435f4e98
Aislan Dias <aislandias@outlook.com>
2024-02-28 13:45:58 -0300

    0204-home-styles

--- 02-timer/src/Router.tsx
+++ 02-timer/src/Router.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home.component'
 import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout/DefaultLayout.component'
 
-export function Router(){
-  return(
+export function Router() {
+  return (
     <Routes>
-      <Route path='/' element={<DefaultLayout />}>
-        <Route path='/' element={<Home/>} />
-        <Route path='/history' element={<History/>} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
       </Route>
     </Routes>
   )
