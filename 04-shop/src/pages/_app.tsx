3233052d38cf614810439b82de6a9c626fd56e8a
Aislan Dias <aislandias@outlook.com>
2024-02-29 17:42:18 -0300

    [styles] application header

--- 04-shop/src/pages/_app.tsx
+++ 04-shop/src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import { globalStyles } from "@/styles/global"
+import logoImg from '../assets/logo.svg'
+import Image from "next/image";
+import { Container, Header } from "@/styles/pages/app.styles";
 
 globalStyles();
 
@@ -10,7 +13,14 @@ export default function App({
 
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Container>
+          <Header>
+            <Image src={logoImg} alt="" />
+          </Header>
+          {children}
+        </Container>
+      </body>
     </html>
   )
 }
