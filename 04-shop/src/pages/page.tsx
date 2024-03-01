1dc5d3117a937ee1ebbb70fa7cfe0aeb06c96b13
Aislan Dias <aislandias@outlook.com>
2024-03-01 13:48:37 -0300

    [feat] SSR rendeing concepts

--- 04-shop/src/pages/page.tsx
+++ 04-shop/src/pages/page.tsx
@@ -9,6 +9,7 @@ import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
 
 import 'keen-slider/keen-slider.min.css'
+import { useEffect, useState } from "react"
 
 const Button = styled('button', {
   backgroundColor: '$green500',
@@ -25,7 +26,8 @@ const Button = styled('button', {
   }
 })
 
-export default function Home() {
+export default function Home(props) {
+  // const [list, setList] = useState<number[]>([])
 
   const [sliderRef] = useKeenSlider({
     slides: {
@@ -34,8 +36,16 @@ export default function Home() {
     }
   })
 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setList([1, 2, 3])
+  //   }, 2000)
+  // }, [])
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
         <footer>
@@ -68,6 +78,16 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
 
 // Filesystem routing
