4b04e7755cca1119773a60434b0dff1ef2ec1a6c
Aislan Dias <aislandias@outlook.com>
2024-03-03 16:07:16 -0300

    0706-transactions-table

--- 03-dt-money/src/pages/Transactions/transactions.component.tsx
+++ 03-dt-money/src/pages/Transactions/transactions.component.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header/header.component";
 import { Summary } from "../Summary/summary.component";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./transactions.styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
