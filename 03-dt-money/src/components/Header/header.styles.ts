51f7fa49267c4bb8779285e6901cd2f3ef735466
Aislan Dias <aislandias@outlook.com>
2024-03-04 15:16:11 -0300

    0709-new-transaction-model

--- 03-dt-money/src/components/Header/header.styles.ts
+++ 03-dt-money/src/components/Header/header.styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+// import * as Dialog from '@radix-ui/react-dialog'
 
 export const HeaderContainer = styled.header`
   background: ${props => props.theme["gray-900"]};
@@ -15,18 +16,19 @@ export const HeaderContent = styled.div`
   align-items: center;
 `
 
+// export const NewTransactionButton = styled(Dialog.Trigger)`
 export const NewTransactionButton = styled.button`
-  height: 50px;
-  border: 0;
-  background: ${props => props.theme["green-500"]};
-  color: ${props => props.theme.white};
-  font-weight: bold;
-  padding: 0 1.25rem;
-  border-radius: 6px;
-  cursor: pointer;
-  transition: background-color 0.2s;
+height: 50px;
+border: 0;
+background: ${props => props.theme["green-500"]};
+color: ${props => props.theme.white};
+font - weight: bold;
+padding: 0 1.25rem;
+border - radius: 6px;
+cursor: pointer;
+transition: background - color 0.2s;
 
   &:hover {
-    background: ${props => props.theme["green-700"]}
-  }
+  background: ${props => props.theme["green-700"]}
+}
 `
