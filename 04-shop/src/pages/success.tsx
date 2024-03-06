551a6a05af0b3ee077080f18fdebb3944fe1ef2b
Aislan Dias <aislandias@outlook.com>
2024-03-06 15:42:33 -0300

    [feat] success page

--- 04-shop/src/pages/success.tsx
+++ 04-shop/src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer } from "@/styles/pages/product.styles";
+import { SuccessContainer } from "@/styles/pages/success.styles";
+import Link from "next/link";
+
 export default function Product() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho!
+      </p>
+
+      <Link>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
