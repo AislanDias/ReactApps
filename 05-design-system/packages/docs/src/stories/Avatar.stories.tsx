92f332bd64af8450244524a784d5f6535b767e6e
Aislan Dias <aislandias@outlook.com>
2024-03-13 15:42:14 -0300

    [feat] avatar - component

---
+++ 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@aislan/react'
+
+export default {
+  title: 'Typography/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/aislandias.png',
+    alt: 'Aislan Dias',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
