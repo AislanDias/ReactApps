528023ef26fd3703f07c287fef6d6aeb1a04be4b
Aislan Dias <aislandias@outlook.com>
2024-03-31 11:42:30 -0300

    [feat] reset form fields

--- 02-timer/src/pages/Home/Home.component.tsx
+++ 02-timer/src/pages/Home/Home.component.tsx
@@ -27,13 +27,14 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 // Avoid creating too much cascade elements, create new styled components instead
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: { task: '', minutesAmount: 0 },
   })
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
