515aeca138cd1b2584e9c649420b3a47a94b5c1d
Aislan Dias <aislandias@outlook.com>
2024-03-31 16:19:55 -0300

    0501-understanding-contexts

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,18 +1,20 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
-
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
+// import { ThemeProvider } from 'styled-components'
+// import { BrowserRouter } from 'react-router-dom'
+// import { Router } from './Router'
+//
+// import { defaultTheme } from './styles/themes/default'
+// import { GlobalStyle } from './styles/global'
+import { Home } from './Home'
 
 export function App() {
   // We can use ThemeProvider as a way to switch between light and dark themes
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+  return <Home />
+  // return (
+  //   <ThemeProvider theme={defaultTheme}>
+  //     <BrowserRouter>
+  //       <Router />
+  //     </BrowserRouter>
+  //     <GlobalStyle />
+  //   </ThemeProvider>
+  // )
 }
