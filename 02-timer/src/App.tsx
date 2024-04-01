8e8726b1211371a1769b78f71159238ca0575f87
Aislan Dias <aislandias@outlook.com>
2024-04-01 13:54:49 -0300

    0502-converting-to-context

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -1,20 +1,20 @@
-// import { ThemeProvider } from 'styled-components'
-// import { BrowserRouter } from 'react-router-dom'
-// import { Router } from './Router'
-//
-// import { defaultTheme } from './styles/themes/default'
-// import { GlobalStyle } from './styles/global'
-import { Home } from './Home'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   // We can use ThemeProvider as a way to switch between light and dark themes
-  return <Home />
-  // return (
-  //   <ThemeProvider theme={defaultTheme}>
-  //     <BrowserRouter>
-  //       <Router />
-  //     </BrowserRouter>
-  //     <GlobalStyle />
-  //   </ThemeProvider>
-  // )
+
+  return (
+    // Theme Provider is a context provider
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
--- 02-timer/src/pages/Home/components/Countdown/Countdown.component.tsx
+++ 02-timer/src/pages/Home/components/Countdown/Countdown.component.tsx
@@ -1,12 +1,7 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { CountdownContainer, Separator } from './Countdown.styles'
 import { differenceInSeconds } from 'date-fns'
-
-interface CountdownProps {
-  activeCycle: any
-  setCycles: any
-  activeCycleId: any
-}
+import { CyclesContext } from '../../Home.component'
 
 /**
  *
@@ -15,11 +10,9 @@ interface CountdownProps {
  * Context API -> Allow us to share information between a lot of components
  */
 
-export function Countdown({
-  activeCycle,
-  setCycles,
-  activeCycleId,
-}: CountdownProps) {
+export function Countdown() {
+  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
+    useContext(CyclesContext)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
@@ -35,16 +28,7 @@ export function Countdown({
         )
 
         if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-              } else {
-                return cycle
-              }
-            }),
-          )
-
+          markCurrentCycleAsFinished()
           setAmountSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
@@ -57,7 +41,19 @@ export function Countdown({
     return () => {
       clearInterval(interval)
     }
-  }, [activeCycle, totalSeconds, activeCycleId])
+  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) document.title = `${minutes}:${seconds}`
+  }, [activeCycle, minutes, seconds])
 
   return (
     <CountdownContainer>
