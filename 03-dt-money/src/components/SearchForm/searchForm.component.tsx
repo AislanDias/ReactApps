7ee94353e66bda71ba34bf817c5483d2db723d74
Aislan Dias <aislandias@outlook.com>
2024-04-13 15:44:47 -0300

    [feat] change searchFormComponent rendering -> memo

--- 03-dt-money/src/components/SearchForm/searchForm.component.tsx
+++ 03-dt-money/src/components/SearchForm/searchForm.component.tsx
@@ -1,18 +1,38 @@
 import { MagnifyingGlass } from 'phosphor-react'
 import { SearchFormContainer } from './searchForm.styles'
+import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../contexts/Transactions.context'
-import { z } from 'zod'
+import { memo } from 'react'
 
 const searchFormSchema = z.object({
   query: z.string(),
 })
 
+/**
+ * Why a component re-renders?
+ *
+ * - Hooks changed
+ * - Props changed
+ * - Parent re-rendered
+ *
+ * What is the rendering pipeline?
+ *
+ * 1. React recreates the HTML of that component
+ * 2. Compare the recreated HTML version with the previous one
+ * 3. If something changed, rewrites the HTML on-screen
+ *
+ * Memo:
+ * 0. Hooks changed, Props changed (deep comparison)
+ * 0.1 Compares if the previous version of Props and Hooks changed
+ * 0.2 If something changed, it'll allow a re-render
+ * */
+
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -51,3 +71,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
--- 03-dt-money/src/contexts/Transactions.context.tsx
+++ 03-dt-money/src/contexts/Transactions.context.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { api } from '../lib/axios'
 import { createContext } from 'use-context-selector'
 
@@ -37,7 +37,7 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     // // fetch('http://localhost:3333/transactions')
     // //   .then(response => response.json())
     // //   .then(data => {
@@ -54,21 +54,27 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
+  // If a function is inside useCallback it will never be re-rendered
+  // Be careful! If the function needs something that is updated,
+  // put the condition in the brackets to trigger a re-rendering
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
 
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
 
-    setTransactions((state) => [response.data, ...state])
-  }
+      setTransactions((state) => [response.data, ...state])
+    },
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions()
